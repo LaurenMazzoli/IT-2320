92364a29beb7fe1ae78761806bb68ea8cb38f70e
LaurenMazzoli <mazzolilauren@gmail.com>
2016-10-24 17:30:49 -0400

    Assignment6

---
+++ Assignment8/Scripts/Home.js
@@ -0,0 +1,29 @@
+﻿var Client = {}
+
+Client.CallGetPlayerInformation = function()
+{
+
+  $.ajax
+    ({
+        url: "Home/GetPlayerInformation",
+        data: { PlayerNumber: $(".player-number-textbox-input").val() },
+        success: function (result)
+        {
+           var player = JSON.parse(result);
+           Client.DisplayPlayerInformation(player);
+
+        }
+    });
+
+}
+Client.DisplayPlayerInformation = function(player)
+{
+    $(".output").text(player.PlayerName);
+} 
+
+$(document).ready(function () 
+
+{
+    $(".player-number-button").click(Client.CallGetPlayerInformation);
+
+});
