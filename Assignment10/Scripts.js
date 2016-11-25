df8c345ee57941e3d9425e99e539185c7b0124fe
LaurenMazzoli <mazzolilauren@gmail.com>
2016-11-25 17:11:31 -0500

    updated

--- Assignment10/Scripts.js
+++ Assignment10/Scripts.js
@@ -4,14 +4,22 @@ Main.Canvas = document.getElementById('myCanvas');
 Main.Context = Main.Canvas.getContext('2d');
 
 Main.MousePressed = false;
+Main.CarouselIsSelected = false;
 Main.MX = 0;
 Main.MY = 0;
 
 Main.MouseUp = function (mouseEvent) {
     Main.MousePressed = false;
+    Main.CarouselIsSelected = false;
 }
 Main.MouseDown = function(mouseEvent) {
+//    check to see if the click is inside a box. 
     Main.MousePressed = true;
+    for (var i = 0; i < Main.Boxes.length; i++)
+    {
+        if (Main.Boxes[i].IsSelected) 
+            Main.CarouselIsSelected = true;
+    }
 }
 Main.Canvas.onmousemove = function (event) {
     if (event.offsetX)
@@ -57,7 +65,7 @@ Main.Box = function(x, y, w, h, img)
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 	this.IsSelected = function () {
-	    if (!Main.Mousedown) return false;
+	    if (!Main.MousePressed) return false;
 	    var boxX2 = this.X + this.Width;
 	    var boxY2 = this.Y + this.Height;
 
@@ -65,8 +73,17 @@ Main.Box = function(x, y, w, h, img)
 	    var insideBoxHeight = Main.MY > this.Y && Main.MY < boxY2;
 	    return insideBoxWidth && insideBoxHeight;
 	}
-	
     ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+	this.IsMovingLeft = function () {
+	    var boxX2 = this.X + this.Width;
+	    var boxY2 = this.Y + this.Height;
+
+	    var insideBoxWidth = Main.MX > this.X && Main.MX < boxX2;
+	    var insideBoxHeight = Main.MY > this.Y && Main.MY < boxY2;
+	    return insideBoxWidth && insideBoxHeight;
+	}
+
     ////////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 	this.DrawAsImage = function()
@@ -109,31 +126,34 @@ Main.Animate = function()
     // check if img selected and if moved left or right from selection
 	var x2 = 0;
 	for (var i=0; i < Main.Boxes.length; i++)
-	{	 
-   	    x2 = Main.Boxes[i].x + Main.Boxes[i].Width;
-   	    if (Main.Boxes[i].IsSelected() && (Main.MX < Main.Boxes[i].x)) 
+	{
+	    
+   	    x2 = Main.Boxes[i].X + Main.Boxes[i].Width;
+   	    if (Main.CarouselIsSelected && Main.MX < Main.Boxes[i].X)
    	        Main.ShiftImagesLeft();
-   	    else if (Main.Boxes[i].IsSelected() && (Main.MX > x2))    
-   	        Main.ShiftImagesRight();   	   
+   	    else if (Main.CarouselIsSelected && (Main.MX > x2))    
+   	        Main.ShiftImagesRight();
    	    Main.Boxes[i].DrawAsImage(Main.Boxes[i].Pic);
 	}
 	requestAnimFrame(function() { Main.Animate(); });
 }
 Main.ShiftImagesLeft=function()
 {
-    alert("in shift left");
-    var tempPic = Main.Boxes[0].Pic;
+  //  alert("in shift left");
+    var tempPic = Main.Boxes[0].Pic.src;
+ //   alert('temp pic src:' + tempPic);
         for (var i = 0; i< Main.Boxes.length-1; i++)
-            Main.Boxes[i].Pic = Main.Boxes[i+1].Pic;
-        Main.Boxes[Main.Boxes.length-1].Pic = tempPic;
+            Main.Boxes[i].Pic.src = Main.Boxes[i + 1].Pic.src;
+        Main.Boxes[Main.Boxes.length-1].Pic.src = tempPic;
 }
 Main.ShiftImagesRight = function()
 {
-        alert('in shift right');
-        var tempPic = Main.Boxes[Main.Boxes.length-1].Pic;
-        for (var i = Main.Boxes.length-2; i > 0; i--)
-            Main.Boxes[i+1].Pic = Main.Boxes[i].Pic;
-        Main.Boxes[0].Pic = tempPic;
+      //  alert('in shift right');
+        var tempPic = Main.Boxes[Main.Boxes.length-1].Pic.src;
+ //       alert('temp pic src:' + tempPic);
+        for (var i = Main.Boxes.length - 2; i > 0; i--)
+            Main.Boxes[i+1].Pic.src = Main.Boxes[i].Pic.src;
+        Main.Boxes[0].Pic.src = tempPic;
 }
 window.requestAnimFrame = (function(callback)
 {
